5fa4947c7924875f45b9a6b10784034704b2cf38
jmuldvp <jmul.dvp@gmail.com>
2016-09-06 18:57:45 -0600

    unable to get to #show after login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  # protected
+  # def after_sign_in_path_for(user)
+  #   users_show_path(current_user)
+  # end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,25 @@
 Rails.application.routes.draw do
+  get 'users/show'
+
   devise_for :users
+  # resources :users, only: [:show]
+
+  authenticated :user do
+    root 'users/show', as: :authenticated_root
+  end
+  # get 'users/:id' => 'users#show', as: :user
+  # get 'users/:id', to: 'users#show'
+
   get 'welcome/index'
 
   get 'welcome/about'
+  # get 'about' => 'welcome#about'  #cp 31
 
   get 'welcome/contact'
 
   root 'welcome#index'
+
+  # root 'users/show'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
