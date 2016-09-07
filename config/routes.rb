26366b09245696684ac86a1886a2a9daccad6d31
jmuldvp <jmul.dvp@gmail.com>
2016-09-07 12:23:06 -0600

    User profile completed

--- config/routes.rb
+++ config/routes.rb
@@ -1,23 +1,22 @@
 Rails.application.routes.draw do
-  get 'users/show'
-
-  devise_for :users
   # resources :users, only: [:show]
-
-  authenticated :user do
-    root 'users/show', as: :authenticated_root
-  end
+  devise_for :users
+  # authenticated :user do
+  #   root 'users#show', as: :authenticated_root
+  # end
   # get 'users/:id' => 'users#show', as: :user
   # get 'users/:id', to: 'users#show'
 
-  get 'welcome/index'
+  # get 'welcome/index'
+  root 'welcome#index'
 
   get 'welcome/about'
   # get 'about' => 'welcome#about'  #cp 31
 
-  get 'welcome/contact'
+  # get 'welcome/contact'
 
-  root 'welcome#index'
+  # root 'welcome#home'
+  get 'users/show'
 
   # root 'users/show'
 
