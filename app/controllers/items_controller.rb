f84a0973a1f7d9c40ebc9c92ce6d528310308589
Brittany Martin <brittany.jill.martin@gmail.com>
2016-09-15 06:08:20 -0400

    Replace AJAX in the Create method on Items

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,37 +1,27 @@
 class ItemsController < ApplicationController
   def create
-    @user = User.find(params[:user_id])
+    @user = User.find_by(params[:user_id])
     @item = @user.items.new(item_params)
     @new_item = Item.new
 
     if @item.save
-      flash[:notice] = "Item was saved successfully."
-      redirect_to [@user]
-    else
-      flash.now[:alert] = "There was an error saving the item. Please try again."
       redirect_to [@user]
     end
   end
 
   def destroy
-    @user = current_user
+    @user = User.find_by(params[:user_id])
     @item = @user.items.find(params[:id])
 
     if @item.destroy
-      flash[:notice] = "Item has been completed!"
-    else
-      flash[:alert] = "There was an error completing the item. Try again."
-    end
-
-    respond_to do |format|
-      format.html
-      format.js
+      respond_to do |format|
+        format.html
+        format.js
+      end
     end
   end
 
-
   private
-
   def item_params
     params.require(:item).permit(:name)
   end
