00f727bcc16ca2ebd3fb30f2f0b5e87b06361a9b
jmuldvp <jmul.dvp@gmail.com>
2016-09-22 17:41:23 -0600

    Completed CP 6 seeding

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,26 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create!( email: "user@user.com", password: "pas123", password_confirmation: "pas123", confirmed_at: Date.today)
+
+
+20.times do
+  User.create!(
+    email:                 Faker::Internet.email,
+    password:              "pas123",
+    password_confirmation: "pas123",
+    confirmed_at: Date.today
+  )
+end
+users = User.all
+
+120.times do
+  Item.create!(
+    name: Faker::Company.bs, # I'd rather see this than Lorem ipsum
+    user: users.sample
+  )
+end
+
+puts "Seed finished"
+puts "#{User.count} users created."
+puts "#{Item.count} items created."
